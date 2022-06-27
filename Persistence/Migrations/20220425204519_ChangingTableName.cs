12d73331feb3fced1e52de2975cbfe3ff0d8329f
koda5 <lkodovski@gmail.com>
2022-06-27 14:45:40 +0200

    ...

---
+++ Persistence/Migrations/20220425204519_ChangingTableName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class ChangingTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activity",
+                table: "Activity");
+
+            migrationBuilder.RenameTable(
+                name: "Activity",
+                newName: "Activities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities");
+
+            migrationBuilder.RenameTable(
+                name: "Activities",
+                newName: "Activity");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activity",
+                table: "Activity",
+                column: "Id");
+        }
+    }
+}
